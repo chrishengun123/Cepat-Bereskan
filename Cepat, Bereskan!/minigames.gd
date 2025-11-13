88b3dbea44b7d7dd6871c2d589a80f4bbc2bb67f
chrishengun123 <chico.gunawan@gmail.com>
2025-11-14 01:36:20 +0700

    Minigames Data Added

---
+++ Cepat, Bereskan!/minigames.gd
@@ -0,0 +1,10 @@
+extends Node
+class_name Minigames
+
+const locations:Dictionary = {
+	"placeholder": [Vector2(64*1,64*1),Vector2(64*2,64*2),Vector2(64*3,64*3),Vector2(64*4,64*4),Vector2(64*5,64*5)]
+}
+
+const textures:Dictionary = {
+	"placeholder": preload("res://icon.svg")
+}
