d56c717c423180bc3ce069fdb5c3065cbf42c5dd
chrishengun123 <chico.gunawan@gmail.com>
2025-11-17 02:49:01 +0700

    Added Pause Menu

--- Cepat, Bereskan!/scripts/ui.gd
+++ Cepat, Bereskan!/scripts/ui.gd
@@ -5,6 +5,17 @@ var game:Game
 
 func _ready() -> void:
 	game = get_parent()
+	$pause_menu.visible = false
 
 func _process(delta: float) -> void:
 	$time_left.text = str(int(get_parent().time_left/60))+":"+str(int(get_parent().time_left)%60)
+	if Input.is_action_just_pressed("pause"):
+		$pause_menu.visible = !$pause_menu.visible
+
+
+func _on_continue_pressed() -> void:
+	$pause_menu.visible = false
+
+
+func _on_give_up_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
