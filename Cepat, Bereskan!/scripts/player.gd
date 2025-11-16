bb39a06e197a09f25c52e84808f615a894d98000
chrishengun123 <chico.gunawan@gmail.com>
2025-11-17 01:41:04 +0700

    Combined X and Y Directions Into One Vector2

--- Cepat, Bereskan!/scripts/player.gd
+++ Cepat, Bereskan!/scripts/player.gd
@@ -3,23 +3,22 @@ extends CharacterBody2D
 @export var Character: Node2D
 
 func _physics_process(delta: float) -> void:
-	var dir_x = Input.get_axis("ui_left", "ui_right") # Change these to wsad
-	var dir_y = Input.get_axis("ui_up", "ui_down")
-	move_and_collide(Vector2(Consts.SPEED * dir_x, Consts.SPEED * dir_y) * delta)
+	var direction:Vector2 = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	move_and_collide(Consts.SPEED * direction * delta)
 
-	rotation(dir_x, dir_y)
+	rotation(direction)
 
-func rotation(dir_x: float, dir_y: float) -> void:
+func rotation(direction:Vector2) -> void:
 	var sprite = Character.get_node("AnimatedSprite2D")
 
-	if dir_x > 0:
+	if direction.x > 0:
 		rotation_degrees = 0
 		sprite.flip_v = false # Use the left/right sprite when made (temporary)
-	elif dir_x < 0:
+	elif direction.x < 0:
 		rotation_degrees = 180
 		sprite.flip_v = true
 	
-	if dir_y > 0:
+	if direction.y > 0:
 		rotation_degrees = 90
-	elif  dir_y < 0:
+	elif  direction.y < 0:
 		rotation_degrees = 270
