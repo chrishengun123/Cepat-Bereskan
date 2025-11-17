ea98a60a740c0406fdbf611bf2ccfc5f40ab4419
chrishengun123 <chico.gunawan@gmail.com>
2025-11-17 16:53:32 +0700

    Replaced Placeholder Task with Sweeping Task

--- Cepat, Bereskan!/scripts/minigames.gd
+++ Cepat, Bereskan!/scripts/minigames.gd
@@ -2,13 +2,13 @@ extends Node
 class_name Minigames
 
 const locations:Dictionary = {
-	"placeholder": [Vector2(500,300), Vector2(600,300), Vector2(700,300), Vector2(500,400), Vector2(600,400), Vector2(700,400), Vector2(500,500), Vector2(600,500), Vector2(700,500)]
+	"sweeping": [Vector2(500,300), Vector2(600,300), Vector2(700,300), Vector2(500,400), Vector2(600,400), Vector2(700,400), Vector2(500,500), Vector2(600,500), Vector2(700,500)]
 }
 
 const textures:Dictionary = {
-	"placeholder": preload("res://icon.svg")
+	"sweeping": preload("res://icon.svg")
 }
 
 const scenes:Dictionary = {
-	
+	"sweeping": preload("res://scenes/sweeping.tscn")
 }
