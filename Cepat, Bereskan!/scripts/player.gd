bf0d2ddae643e2f32bcfacd88b362f98998ba92f
chrishengun123 <chico.gunawan@gmail.com>
2025-11-17 17:05:17 +0700

    Updated Player Sprite, Movement, and Collision

--- Cepat, Bereskan!/scripts/player.gd
+++ Cepat, Bereskan!/scripts/player.gd
@@ -6,18 +6,12 @@ class_name Player
 func _physics_process(delta: float) -> void:
 	var direction:Vector2 = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	move_and_collide(Consts.SPEED * direction * delta)
-
 	rotation(direction)
 
 func rotation(direction:Vector2) -> void:
-	if direction.x > 0:
-		rotation_degrees = 0
-		character.flip_v = false # Use the left/right sprite when made (temporary)
-	elif direction.x < 0:
-		rotation_degrees = 180
-		character.flip_v = true
-	
-	if direction.y > 0:
-		rotation_degrees = 90
-	elif  direction.y < 0:
-		rotation_degrees = 270
+	if direction.x < 0:
+		character.play("left")
+	elif direction.x > 0:
+		character.play("right")
+	else:
+		character.stop()
