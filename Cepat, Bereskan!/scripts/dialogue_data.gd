dee56a942d515346a926defded3c5b0c3a931d37
chrishengun123 <chico.gunawan@gmail.com>
2025-11-17 20:49:32 +0700

    Added Dialogue System

---
+++ Cepat, Bereskan!/scripts/dialogue_data.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name DialogueData
+
+var scripts:Dictionary = {
+	"good ending": [""]
+}
--- Cepat, Bereskan!/scripts/ui.gd
+++ Cepat, Bereskan!/scripts/ui.gd
@@ -2,16 +2,32 @@ extends CanvasLayer
 class_name UI
 
 var game:Game
+var dialogue_data:DialogueData = DialogueData.new()
+var dialogue:Array = []
+var dialogue_index:int = 0
 
 func _ready() -> void:
 	game = get_parent()
 	$pause_menu.visible = false
+	print(dialogue_data.scripts)
 
 func _process(delta: float) -> void:
-	$time_left.text = str(int(get_parent().time_left/60))+":"+str(int(get_parent().time_left)%60)
-	if Input.is_action_just_pressed("pause"):
-		$pause_menu.visible = !$pause_menu.visible
+	if dialogue:
+		if Input.is_action_just_pressed("next"):
+			if dialogue_index > dialogue.size():
+				dialogue = []
+			else:
+				%dialogue_text.text = dialogue[dialogue_index]
+				dialogue_index += 1
+	else:
+		$time_left.text = str(int(get_parent().time_left/60))+":"+str(int(get_parent().time_left)%60)
+		if Input.is_action_just_pressed("pause"):
+			$pause_menu.visible = !$pause_menu.visible
 
+func start_dialogue(title):
+	get_tree().paused = true
+	dialogue = dialogue_data.scripts.get(title)
+	dialogue_index = 0
 
 func _on_continue_pressed() -> void:
 	$pause_menu.visible = false
