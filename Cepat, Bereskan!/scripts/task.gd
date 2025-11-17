5364dc78320b3a7193abdb448deb27c18697effe
chrishengun123 <chico.gunawan@gmail.com>
2025-11-17 16:43:24 +0700

    Updated Task Sprite System

--- Cepat, Bereskan!/scripts/task.gd
+++ Cepat, Bereskan!/scripts/task.gd
@@ -2,17 +2,24 @@ extends Sprite2D
 class_name Task
 
 var minigames:Minigames = Minigames.new()
+var game:Game
 var type:String
 var player_touching:bool = false
-var task_minigame
+var task_minigame:Node
+var task_started:bool = false
 
 func _ready() -> void:
 	material = material.duplicate()
+	game = get_parent()
 
 func _process(delta: float) -> void:
 	if player_touching and Input.is_action_just_pressed("ui_accept"):
 		task_minigame = minigames.scenes.get(type).instantiate()
-		get_parent().minigame.add_child()
+		game.minigame.add_child(task_minigame)
+		task_started = true
+	if task_started and !task_minigame:
+		game.tasks_left.erase(self)
+		queue_free()
 
 
 
