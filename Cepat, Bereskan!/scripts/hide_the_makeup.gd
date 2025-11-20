796cf4360d7f48acee99b784e6d6690c2e81f7a1
chrishengun123 <chico.gunawan@gmail.com>
2025-11-20 21:23:04 +0700

    Hide the Makeup

--- Cepat, Bereskan!/scripts/hide_the_makeup.gd
+++ Cepat, Bereskan!/scripts/hide_the_makeup.gd
@@ -1,6 +1,7 @@
 extends Node2D
 class_name HideTheMakeup
 
+@onready var bag_space:TileMapLayer = $tilemap/bag_space
 var makeup_data = preload("res://scripts/makeup_data.gd")
 var makeup:Array
 const sides:Array = [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.UP, Vector2i.DOWN]
@@ -11,10 +12,10 @@ func _ready() -> void:
 	start(10)
 
 func start(amount:int):
-	$bag_space.clear()
+	bag_space.clear()
 	var possible_patterns:Array = []
-	for i in range($bag_space.tile_set.get_patterns_count()):
-		possible_patterns.append($bag_space.tile_set.get_pattern(i))
+	for i in range(bag_space.tile_set.get_patterns_count()):
+		possible_patterns.append(bag_space.tile_set.get_pattern(i))
 	var possible_coords:Array = []
 	for y in max_bag_size.y:
 		for x in max_bag_size.x:
@@ -27,8 +28,8 @@ func start(amount:int):
 				start(amount)
 				return
 			var makeup_pattern:TileMapPattern = unchecked_patterns.pop_at(randi()%unchecked_patterns.size())
-			if !$bag_space.get_used_rect().size:
-				$bag_space.set_pattern(Vector2i(5,5),makeup_pattern)
+			if !bag_space.get_used_rect().size:
+				bag_space.set_pattern(Vector2i(5,5),makeup_pattern)
 				success = true
 			else:
 				var finished = false
@@ -42,24 +43,24 @@ func start(amount:int):
 						else:
 							var rand_coords = unchecked_coords.pop_at(randi()%unchecked_coords.size())
 							for cell_pos in makeup_pattern.get_used_cells():
-								if (cell_pos+rand_coords).x >= max_bag_size.x or (cell_pos+rand_coords).y >= max_bag_size.y or $bag_space.get_cell_atlas_coords(cell_pos+rand_coords) != Vector2i(-1,-1):
+								if (cell_pos+rand_coords).x >= max_bag_size.x or (cell_pos+rand_coords).y >= max_bag_size.y or bag_space.get_cell_atlas_coords(cell_pos+rand_coords) != Vector2i(-1,-1):
 									placed = false
 							if !has_connection(makeup_pattern, rand_coords) or produces_hole(makeup_pattern, rand_coords):
 								placed = false
 							if placed:
-								$bag_space.set_pattern(rand_coords,makeup_pattern)
+								bag_space.set_pattern(rand_coords,makeup_pattern)
 								finished = true
 								success = true
 
 func has_connection(pattern:TileMapPattern, pos:Vector2i):
 	for i in range(pattern.get_used_cells().size()):
 		for direction in sides:
-			if $bag_space.get_cell_atlas_coords(pattern.get_used_cells()[i]+pos+direction) != -Vector2i.ONE:
+			if bag_space.get_cell_atlas_coords(pattern.get_used_cells()[i]+pos+direction) != -Vector2i.ONE:
 				return true
 	return false
 
 func produces_hole(pattern:TileMapPattern, pos:Vector2i):
-	var gen_result:Array = $bag_space.get_used_cells()
+	var gen_result:Array = bag_space.get_used_cells()
 	var addition:Array = pattern.get_used_cells()
 	for i in range(addition.size()):
 		addition[i] += pos
