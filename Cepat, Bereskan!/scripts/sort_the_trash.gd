393643587785beb85b51c94ea6e077ff5203049a
chrishengun123 <chico.gunawan@gmail.com>
2025-11-23 10:01:27 +0700

    Sort the Trash Update

--- Cepat, Bereskan!/scripts/sort_the_trash.gd
+++ Cepat, Bereskan!/scripts/sort_the_trash.gd
@@ -1,14 +1,24 @@
 extends Node2D
 class_name SortTheTrash
 
-var trash_scene = preload("res://scenes/trash.tscn")
 var trash_data = preload("res://scripts/trash_data.gd")
 var unsorted:Array = []
 var sorted:Array = []
 
 func _ready() -> void:
 	for i in range(10):
-		var trash:Trash = trash_scene.instantiate()
+		var trash:Trash = Trash.new()
 		trash.type = ["organic", "inorganic"].pick_random()
 		trash.texture = trash_data.textures.get(trash.type).pick_random()
 		trash.position = Vector2(randf_range(56,1088),576)
+		unsorted.append(trash)
+		add_child(trash)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("click"):
+		for trash:Trash in unsorted:
+			if trash.get_rect().has_point(to_local(event.position)):
+				trash.held = true
+	elif Input.is_action_just_released("click"):
+		for trash:Trash in unsorted:
+			trash.held = false
--- Cepat, Bereskan!/scripts/trash.gd
+++ Cepat, Bereskan!/scripts/trash.gd
@@ -2,3 +2,8 @@ extends Sprite2D
 class_name Trash
 
 var type:String
+var held:bool = false
+
+func _process(delta: float) -> void:
+	if held:
+		position = get_global_mouse_position()
