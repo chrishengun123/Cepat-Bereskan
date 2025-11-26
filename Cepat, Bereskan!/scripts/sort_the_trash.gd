b0dc7fe9acf9d4e49621851aff1555aaf5dbf04c
ItsOwly <itsnot.owly@gmail.com>
2025-11-26 17:41:02 +0700

    Updated trash sorting game

--- Cepat, Bereskan!/scripts/sort_the_trash.gd
+++ Cepat, Bereskan!/scripts/sort_the_trash.gd
@@ -5,8 +5,7 @@ var trash_data = preload("res://scripts/trash_data.gd").new()
 var unsorted:Array = []
 var sorted:Array = []
 
-@onready var organic_bin = $organic_bin
-@onready var inorganic_bin = $inorganic_bin
+@onready var bins: Array = [$OrganicBin, $InorganicBin]
 
 func _ready() -> void:
 	for i in range(10):
@@ -27,18 +26,49 @@ func _unhandled_input(event: InputEvent) -> void:
 				break
 	elif Input.is_action_just_released("click"):
 		for trash:Trash in unsorted:
-			trash.held = false
-			if is_overlapping(trash, organic_bin) and trash.type == "organic":
-				unsorted.erase(trash)
-				trash.queue_free()
-			if is_overlapping(trash, inorganic_bin) and trash.type == "inorganic":
-				unsorted.erase(trash)
-				trash.queue_free()
+			if trash.held:
+				var timer:Timer = Timer.new()
+				add_child(timer)
+				timer.start(0.001)
+				await timer.timeout
+				timer.queue_free()
+				if trash:
+					trash.held = false
+					trash.position.y = 555
+					if trash.position.x > 1000 or trash.position.x < 150:
+						trash.position.x = randf_range(150,1000)
 
 func _process(float) -> void:
 	if len(unsorted) == 0:
 		queue_free()
 
-func is_overlapping(trash: Sprite2D, bin: Sprite2D) -> bool:
+func is_overlapping_organic(trash: Sprite2D, bin: Sprite2D) -> bool:
 	var local_point = bin.to_local(trash.global_position)
 	return bin.get_rect().has_point(local_point)
+
+func is_overlapping_inorganic(trash: Sprite2D, bin: Sprite2D) -> bool:
+	var local_point = bin.to_local(trash.global_position)
+	return bin.get_rect().has_point(local_point)
+
+
+func _on_organic_hitbox_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_released("click"):
+		for trash:Trash in unsorted:
+			if trash.held:
+				if trash.type == $OrganicBin.type:
+					unsorted.erase(trash)
+					trash.queue_free()
+				#else:
+					#trash.held = false
+					#trash.position = Vector2(randf_range(150,1000),555)
+
+func _on_inorganic_hitbox_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_released("click"):
+		for trash:Trash in unsorted:
+			if trash.held:
+				if trash.type == $InorganicBin.type:
+					unsorted.erase(trash)
+					trash.queue_free()
+				#else:
+					#trash.held = false
+					#trash.position = Vector2(randf_range(150,1000),555)
